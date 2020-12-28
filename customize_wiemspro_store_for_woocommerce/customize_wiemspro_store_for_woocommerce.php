c1561c06d1cc822cd77980190d4b5d59f774bfa7
Victor Arley Pérez Blanco <victorarley35@gmail.com>
2020-12-28 14:32:40 +0100

    Change name plugin

--- customize_wiemspro_store_for_woocommerce/customize_wiemspro_store_for_woocommerce.php
+++ customize_wiemspro_store_for_woocommerce/customize_wiemspro_store_for_woocommerce.php
@@ -1,28 +1,24 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
+ * Customize Wiemspro Store for Woocommerce
+ * 
+ * Este plugin añade las siguientes funcionalidades al plugin de Woocommerce:
+ *  - Creación de productos personalizados.
+ *  - Llamada a nuestros servicios al terminar una compra.
+ *  - Oculta categorias de productos por roles de usuarios. 
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Customize Wiemspro Store for Woocommerce 
+ * Description:       Este plugin añade las siguientes funcionalidades al plugin de Woocommerce:
+ *  - Creación de productos personalizados.
+ *  - Llamada a nuestros servicios al terminar una compra.
+ *  - Oculta categorias de productos por roles de usuarios.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Wimemspro
+ * Author URI:        https://wiemspro.com/
  * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
- * Domain Path:       /languages
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt 
  */
 
 // If this file is called directly, abort.
